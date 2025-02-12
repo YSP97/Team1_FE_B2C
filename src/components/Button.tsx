b973c6959d667b9fabccc2c8dab9b7d7a4e92a13
Gomyeunghan <rhaudgks04@gmail.com>
2025-02-12 18:39:15 +0900

    [modify] buttonComponent 기본스타일링 및 press 스타일 click style 적용 props 수정

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,26 +1,49 @@
-import { ReactNode } from "react";
+import { TailwindColor, TailwindRounded } from "@/types/buttonTailwindType";
+import { ReactNode, useState } from "react";
 
 type ButtonProps = {
-  rounded?: string;
-  textColor?: string;
-  bgColor?: string;
+  rounded?: TailwindRounded;
+  textColor?: TailwindColor;
+  bgColor?: TailwindColor;
+  isClickTextColor?: TailwindColor;
+  isClickBg?: TailwindColor;
   children: ReactNode;
   onClick?: () => void;
 };
 
 export default function Button({
   rounded = "rounded-[3rem]",
-  textColor = "text-white",
+  textColor = "text-primary",
   bgColor = "bg-primary",
+  isClickTextColor,
+  isClickBg,
   children,
   onClick,
 }: ButtonProps) {
+  const [isClick, setIsClick] = useState(false);
+
+  const handleCLick = () => {
+    if (onClick) onClick();
+    setIsClick(!isClick);
+  };
+
   return (
-    <button
-      className={`text-center ${bgColor} ${textColor} py-6 px-6 ${rounded} font-bold text-lg w-full`}
-      onClick={onClick}
-    >
-      {children}
-    </button>
+    <div>
+      {isClick ? (
+        <button
+          className={`text-center ${isClickBg} ${isClickTextColor} py-6 px-6 ${rounded} font-bold text-lg w-full active:opacity-80 trasition-all duration-300 ease-out`}
+          onClick={handleCLick}
+        >
+          {children}
+        </button>
+      ) : (
+        <button
+          className={`text-center ${bgColor} ${textColor} py-6 px-6 ${rounded} font-bold text-lg w-full active:opacity-80 trasition-all duration-300 ease-out`}
+          onClick={handleCLick}
+        >
+          {children}
+        </button>
+      )}
+    </div>
   );
 }
