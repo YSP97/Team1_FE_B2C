54e945611932ae6122c510f14af389ee934ac1af
Gomyeunghan <rhaudgks04@gmail.com>
2025-02-12 17:35:54 +0900

    [feat] Button component type 파일 설정

---
+++ src/types/buttonTailwindType.ts
@@ -0,0 +1,22 @@
+export const tailwindColors = [
+  "text-primary",
+  "text-navy-dark",
+  "text-gray-100",
+  "bg-primary",
+  "bg-navy-dark",
+] as const;
+
+export const tailwindRounded = [
+  "rounded-none",
+  "rounded-sm",
+  "rounded-md",
+  "rounded-lg",
+  "rounded-xl",
+  "rounded-2xl",
+  "rounded-3xl",
+  "rounded-full",
+  "rounded-[3rem]",
+] as const;
+
+export type TailwindColor = (typeof tailwindColors)[number];
+export type TailwindRounded = (typeof tailwindRounded)[number];
