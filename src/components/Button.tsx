95bfcb21aaa1aab3c2c4384f7521a0c859eb8659
Gomyeunghan <rhaudgks04@gmail.com>
2025-02-12 17:26:31 +0900

    [feat] 버튼컴포넌트 마크업 및 기본스타일링

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,26 @@
-export default function () {
+import { ReactNode } from "react";
+
+type ButtonProps = {
+  rounded?: string;
+  textColor?: string;
+  bgColor?: string;
+  children: ReactNode;
+  onClick?: () => void;
+};
+
+export default function Button({
+  rounded = "rounded-[3rem]",
+  textColor = "text-white",
+  bgColor = "bg-primary",
+  children,
+  onClick,
+}: ButtonProps) {
   return (
-    <div>
-      <button>버튼입니다</button>
-    </div>
+    <button
+      className={`text-center ${bgColor} ${textColor} py-6 px-6 ${rounded} font-bold text-lg w-full`}
+      onClick={onClick}
+    >
+      {children}
+    </button>
   );
 }
