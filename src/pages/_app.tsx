e4c6aa5cfe11a23f0da7004c46b7267afd26a3c4
YSP97 <pus9717@gmail.com>
2025-02-13 11:22:23 +0900

    [Feat] Header 제작 - logo 이미지 public 폴더에 추가 - navigation 리스트 생성 - 모바일 화면 메뉴 생성 - 헤더 제작 - 공통 헤더 적용용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css';
 import localFont from 'next/font/local';
 import type { AppProps } from 'next/app';
+import Header from '@/components/Header/Header';
 
 const pretendard = localFont({
   src: '../fonts/PretendardVariable.woff2',
@@ -12,6 +13,7 @@ const pretendard = localFont({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={`${pretendard.variable} font-pretendard`}>
+      <Header />
       <Component {...pageProps} />
     </main>
   );
