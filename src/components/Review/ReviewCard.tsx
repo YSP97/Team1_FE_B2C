79cca39fdf45a910abf4d858da2b6e08a24d87dd
채소 <chlthgus0901@naver.com>
2025-02-13 16:04:44 +0900

    [Modi] 리뷰카드 스타일링 수정

--- src/components/Review/ReviewCard.tsx
+++ src/components/Review/ReviewCard.tsx
@@ -9,18 +9,19 @@ type ReviewProps = {
 
 export default function ReviewCard({ id, img, rating, content }: ReviewProps) {
   return (
-    <article className=" bg-gray-500 rounded-[6px] flex p-4 flex-col gap-4 self-stretch ">
-      <div className="flex justify-between items-center self-stretch">
+    <article className=" bg-gray-500 rounded-lg flex p-4 flex-col gap-2 self-stretch ">
+      <div className="flex justify-between gap-1 items-center self-stretch">
         <div className="flex gap-2">
           <div className="w-8 h-8 bg-gray-400 rounded-full relative overflow-hidden">
             <Image src="/" alt={img} fill className="object-cover absolute" />
           </div>
-          <span className="text-white self-stretch overflow-hidden overflow-ellipsis text-sm not-italic font-medium leading-8">
+          <span className="overflow-hidden text-white overflow-ellipsis text-[1rem] font-normal leading-[140%]">
             {id}
           </span>
         </div>
+
+        {/* 별점 아이콘 추후 수정 예정 */}
         <div className=" w-[3.64156rem] h-[0.61319rem] flex items-center gap-1">
-          {/* 별점 아이콘 추후 수정 예정 */}
           {Array.from({ length: rating }).map((_, index) => (
             <div
               key={index}
@@ -28,7 +29,8 @@ export default function ReviewCard({ id, img, rating, content }: ReviewProps) {
           ))}
         </div>
       </div>
-      <span className=" text-white self-stretch h-6 overflow-hidden overflow-ellipsis text-base not-italic font-normal leading-6 text-left">
+
+      <span className="overflow-hidden text-white overflow-ellipsis text-[1.125rem] font-normal leading-[140%]">
         {content}
       </span>
     </article>
