600a58fd615c7417930daaea9ed6bc4bea52158a
ttining <ttining.real@gmail.com>
2025-02-14 00:10:05 +0900

    [Modify] 카드 컴포넌트 스타일링 수정

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -11,29 +11,29 @@ interface CardProps {
 
 export default function Card({ title, price, basis, desc, list }: CardProps) {
   return (
-    <article className='flex-1 flex flex-col gap-8 p-8 bg-gray-500 border border-gray-200 rounded-lg hover:border-t-8 hover:border-t-primary box-border hover:scale-105 transition-transform duration-300'>
+    <article className='flex-1 flex flex-col gap-8 p-8 bg-bg-primary border border-gray-200 rounded-lg hover:border-t-8 hover:border-t-primary box-border hover:scale-105 transition-transform duration-300'>
       <dl className='basis-auto flex flex-col gap-1'>
-        <dt className='text-white text-3xl font-medium'>{title}</dt>
+        <dt className='text-white text-lg font-medium'>{title}</dt>
         <dd className='flex items-end space-x-2'>
-          <strong className='text-6xl text-white'>{price}</strong>
-          <span className='pb-1 text-2xl text-white'>/ {basis}</span>
+          <strong className='text-xl text-white'>{price}</strong>
+          <span className='pb-1 text-base text-white'>/ {basis}</span>
         </dd>
-        <dd className='text-white text-lg'>{desc}</dd>
+        <dd className='text-white text-base'>{desc}</dd>
       </dl>
       <ul
         role='list'
-        className='flex-1 flex flex-col pt-6 border-t border-white gap-4'
+        className='flex-1 flex flex-col pt-6 border-t border-white gap-2'
       >
         {list.map((item, index) => (
-          <li key={index} role='listitem' className='flex flex-col gap-2'>
-            <p className='text-2xl text-white font-medium'>
+          <li key={index} role='listitem' className='flex flex-col gap-1'>
+            <p className='text-md text-white'>
               <span aria-hidden='true' className='pr-2'>
                 ✅
               </span>
               {item.text}
             </p>
             {item.sub && (
-              <div className='pl-10 text-lg text-gray-100 font-medium'>
+              <div className='pl-10 text-base text-gray-100 font-medium'>
                 {typeof item.sub === "string" ? (
                   <p>{item.sub}</p>
                 ) : Array.isArray(item.sub) ? (
