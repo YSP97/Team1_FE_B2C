2e64fe54eaeb88b4b1191e77c155be17c8499c0c
채소 <chlthgus0901@naver.com>
2025-02-18 11:24:49 +0900

    [Modify] tailwind.config 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,6 +19,7 @@ export default {
         "primary-red": "rgba(244, 67, 54, 1)",
         "bg-primary": "rgba(36, 36, 36, 1)",
         "bg-secondary": "rgba(46, 46, 46, 1)",
+        "gray-50": "rgba(255, 255, 255, 0.05)",
         "gray-100": "rgba(176, 176, 176, 1)",
         "gray-200": "rgba(108, 108, 108, 1)",
         "gray-300": "rgba(100, 100, 100, 1)",
@@ -36,6 +37,7 @@ export default {
         "2xl": "2.5rem", // 40px
       },
       borderRadius: {
+        xs: "0.375rem", // 6px
         sm: "0.5rem", // 8px
         base: "1rem", // 16px
         md: "2rem", // 32px
