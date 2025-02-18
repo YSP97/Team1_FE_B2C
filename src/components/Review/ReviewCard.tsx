5c31cd0f1c4c8acef2353118713477d592dda5c8
채소 <chlthgus0901@naver.com>
2025-02-18 20:57:10 +0900

    [Modify] Review 스타일링 수정

--- src/components/Review/ReviewCard.tsx
+++ src/components/Review/ReviewCard.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
-import SVGIcon from "../SVGIcon";
+import Image from 'next/image';
+import SVGIcon from '../SVGIcon';
 
 type ReviewProps = {
   id: string;
@@ -10,31 +10,32 @@ type ReviewProps = {
 
 function ReviewCard({ id, img, rating, content }: ReviewProps) {
   return (
-    <article className="flex lg:max-w-[35.125rem] p-4 flex-col rounded-xs bg-gray-50 gap-4 self-stretch">
-      <div className="flex justify-between gap-1 items-center self-stretch">
+    <article className="flex flex-col gap-4 self-stretch rounded-xs bg-gray-50 p-4 md:max-w-[35.125rem]">
+      <div className="flex items-center justify-between gap-1 self-stretch">
         <div className="flex gap-2">
-          <div className="w-8 h-8 bg-gray-400 rounded-full relative overflow-hidden">
-            <Image src="/" alt={img} fill className="object-cover absolute" />
+          <div className="relative h-8 w-8 overflow-hidden rounded-full bg-gray-400">
+            <Image src="/" alt={img} fill className="absolute object-cover" />
           </div>
-          <span className="overflow-hidden text-white overflow-ellipsis text-base font-normal leading-[140%]">
+          <span className="overflow-hidden overflow-ellipsis text-base font-normal leading-[140%] text-white">
             {id}
           </span>
         </div>
         <div
-          className=" w-[3.64156rem] h-[0.61319rem] flex items-center gap-1"
-          aria-label={`5점 만점에 ${rating}점`}>
+          className="flex h-[0.61319rem] w-[3.64156rem] items-center gap-1"
+          aria-label={`5점 만점에 ${rating}점`}
+        >
           {[...Array(5)].map((_, index) => (
             <SVGIcon
               key={index}
               name="icon-favorite"
               size={11}
-              className={index < rating ? "text-yellow-400" : "text-gray-300"}
+              className={index < rating ? 'text-yellow-400' : 'text-gray-300'}
             />
           ))}
         </div>
       </div>
 
-      <span className="overflow-hidden text-white overflow-ellipsis text-[1.125rem] font-normal leading-[140%]">
+      <span className="overflow-hidden overflow-ellipsis text-[1.125rem] font-normal leading-[140%] text-white">
         {content}
       </span>
     </article>
