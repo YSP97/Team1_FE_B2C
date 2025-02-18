44a3fdae161ae95266968349143f105e013b4cbb
채소 <chlthgus0901@naver.com>
2025-02-18 11:33:21 +0900

    [Modify] 리뷰 컴포넌트 스타일링 수정

--- src/components/Review/Review.tsx
+++ src/components/Review/Review.tsx
@@ -1,3 +1,4 @@
+import BundleText from "../BundleText";
 import ReviewCard from "./ReviewCard";
 
 const mockData = [
@@ -9,17 +10,15 @@ const mockData = [
 ];
 
 function Review() {
+  const TEXT = {
+    title1: "실제 참가자들의 다양한 후기",
+    sub1: "재등록율 높은 핏큘레이터",
+  };
+
   return (
-    <div className="h-[592px] mx-4 my-8 flex flex-col lg:flex-row lg:gap-[3.75rem]  lg:h-[31.5rem] lg:w-[80rem] lg:mx-[auto] lg:mt-16 lg:mb-28 lg:items-center">
-      <div className="flex flex-col gap-4 mb-4 lg:gap-6 lg:items-start lg:w-[610px]">
-        <span className="text-white text-center text-2xl lg:text-[2.5rem] font-bold leading-[140%]">
-          실제 참가자들의 다양한 후기
-        </span>
-        <span className="text-gray-100 text-center lg:text-[1.5rem] text-base font-normal leading-[140%]">
-          재등록율 높은 핏큘레이터
-        </span>
-      </div>
-      <div className="h-[500px] lg:w-[618px] custom-scrollbar flex flex-col gap-4 pr-2 py-4">
+    <div className="h-[592px] my-8 m-4 flex flex-col md:flex-row md:gap-[3.75rem]  md:h-[31.5rem] md:max-w-[80rem] md:mx-auto md:mt-16 md:mb-28 md:items-center">
+      <BundleText text={TEXT} />
+      <div className="h-[500px] w-full lg:max-w-[35.125rem] custom-scrollbar flex flex-col gap-4 pr-2 py-4">
         {mockData.map((d) => (
           <ReviewCard
             key={d.id}
