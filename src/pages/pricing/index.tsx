f318c5e39a5dcef3e60fbe6c3ed7ac88f4edd2a2
ttining <ttining.real@gmail.com>
2025-02-18 10:52:35 +0900

    Modify : Pricing 페이지
    - mock 데이터 추가
    - 카드 리스트 렌더링

--- src/pages/pricing/index.tsx
+++ src/pages/pricing/index.tsx
@@ -1,3 +1,30 @@
+import { useCallback, useState } from 'react';
+import Card from './../../components/Card';
+import { mockDataCardList } from '@/mockData/mockCardList';
+
 export default function Pricing() {
-  return <h1>유료 플랜 소개</h1>;
+  const [selectedCard, setSelectedCard] = useState<number | null>(null);
+
+  const handleCardClick = useCallback((id: number) => {
+    setSelectedCard((prev) => (prev === id ? null : id));
+  }, []);
+
+  return (
+    <div className="h-[calc(100vh-48px)] bg-bg-primary md:h-[calc(100vh-60px)]">
+      <h1 className="flex flex-col py-8 text-center text-xl text-white">
+        비대면 운동관리 프로그램{' '}
+        <strong className="text-primary">핏큘레이터 신청하기</strong>
+      </h1>
+      <div className="flex flex-col gap-6 p-6 md:m-auto md:max-w-7xl md:flex-row">
+        {mockDataCardList.map((card) => (
+          <Card
+            key={card.id}
+            {...card}
+            isSelected={selectedCard === card.id}
+            onClick={() => handleCardClick(card.id)}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
