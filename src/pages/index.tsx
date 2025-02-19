65ac6b6ba06d049276fc7e85237d9dd9e5864edd
YSP97 <pus9717@gmail.com>
2025-02-19 19:36:26 +0900

    [Feat] 앱 실사용 섹션 제작 - 데스크탑 애니메이션 적용 - 모바일 애니메이션 적용 - 홈페이지에 섹션 추가 - 스크롤바 없애는 코드 global.css에 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,15 @@
+import dynamic from 'next/dynamic';
+
+const AppIntroduce = dynamic(() => import('../components/AppIntroduce'), {
+  ssr: false,
+});
+
 export default function Home() {
-  return <h1 className="text-3xl">홈페이지</h1>;
+  return (
+    <h1 className="text-3xl">
+      <div className="h-auto">
+        <AppIntroduce />
+      </div>
+    </h1>
+  );
 }
