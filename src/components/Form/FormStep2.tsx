8a70e9b1b9ec953e902545180663a13ef0b851dd
채소 <chlthgus0901@naver.com>
2025-02-20 19:33:09 +0900

    [Feat] FormStep2 컴포넌트 생성

---
+++ src/components/Form/FormStep2.tsx
@@ -0,0 +1,15 @@
+import ExerciseGoal from './ExerciseGoal ';
+import ExerciseLevel from './ExerciseLevel';
+import ReferralSource from './ReferralSource';
+import WearableDevice from './WearableDevice';
+
+export default function FormStep2() {
+  return (
+    <div className="mx-auto mt-12 flex max-w-[20.4375rem] flex-col gap-12 md:mt-[60px] md:max-w-[45rem]">
+      <WearableDevice />
+      <ExerciseGoal />
+      <ExerciseLevel />
+      <ReferralSource />
+    </div>
+  );
+}
