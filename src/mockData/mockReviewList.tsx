699913df869e4956ff84ef091d6f117371f1f5e3
채소 <chlthgus0901@naver.com>
2025-02-20 11:10:01 +0900

    [Modify] mockdata에 이미지 추가

--- src/mockData/mockReviewList.tsx
+++ src/mockData/mockReviewList.tsx
@@ -1,38 +1,43 @@
 export const mockReviewList = [
-  { id: 'user1', img: 'img', rating: 4, content: '너무너무 좋아여' },
+  {
+    id: 'user1',
+    img: '/assets/bike.png',
+    rating: 4,
+    content: '너무너무 좋아여',
+  },
   {
     id: 'user2',
-    img: 'img',
+    img: '/assets/bike.png',
     rating: 4,
     content:
       '핏큘레이터 짱짱 핏큘레이터를 만나고 인생이 달라졌어요 최고핏큘레이터를 만나고 인생이 달라졌어요 최고핏큘레이터를 만나고 인생이 달라졌어요 최고핏큘레이터를 만나고 인생이 달라졌어요 최고',
   },
   {
     id: 'user3',
-    img: 'img',
+    img: '/assets/bike.png',
     rating: 5,
     content:
       '핏큘레이터를 만나고 인생이 달라졌어요 최고 핏큘레이터를 만나고 인생이 달라졌어요 최고 핏큘레이터를 만나고 인생이 달라졌어요 최고 핏큘레이터를 만나고 인생이 달라졌어요 최고 핏큘레이터를 만나고 인생이 달라졌어요 최고',
   },
   {
     id: 'user4',
-    img: 'img',
+    img: '/assets/bike.png',
     rating: 5,
     content: '최고에요 핏큘레이터를 만나고 인생이 달라졌어요 최고',
   },
   {
     id: 'user5',
-    img: 'img',
+    img: '/assets/bike.png',
     rating: 4,
     content:
       '핏큘레이터 없이 몬산다 핏큘레이터를 만나고 인생이 달라졌어요 최고핏큘레이터를 만나고 인생이 달라졌어요 최고핏큘레이터를 만나고 인생이 달라졌어요 최고핏큘레이터를 만나고 인생이 달라졌어요 최고핏큘레이터를 만나고 인생이 달라졌어요 최고핏큘레이터를 만나고 인생이 달라졌어요 최고',
   },
   {
     id: 'user6',
-    img: 'img',
+    img: '/assets/bike.png',
     rating: 4,
     content:
       '그룹운동은 핏큘레이터~~ 핏큘레이터를 만나고 인생이 달라졌어요 최고핏큘레이터를 만나고 인생이 달라졌어요 최고핏큘레이터를 만나고 인생이 달라졌어요 최고핏큘레이터를 만나고 인생이 달라졌어요 최고핏큘레이터를 만나고 인생이 달라졌어요 최고핏큘레이터를 만나고 인생이 달라졌어요 최고핏큘레이터를 만나고 인생이 달라졌어요 최고',
   },
-  { id: 'user7', img: 'img', rating: 5, content: '최고~~' },
+  { id: 'user7', img: '/assets/bike.png', rating: 5, content: '최고~~' },
 ];
