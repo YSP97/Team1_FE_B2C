6a30867c96602c69982ea7f3d2b8b73b3380d813
ttining <ttining.real@gmail.com>
2025-02-21 10:07:35 +0900

    Bugfix : useModal 훅 수정
    - 모달 닫았을 때 스크롤 안 생기는 현상 수정

--- src/hooks/useModal.tsx
+++ src/hooks/useModal.tsx
@@ -1,24 +1,11 @@
 import { useEffect } from 'react';
 
-let openModalCount = 0;
-
 function useModal(isOpened: boolean) {
   useEffect(() => {
-    if (isOpened) {
-      openModalCount += 1;
-      document.body.style.overflow = 'hidden';
-    } else if (openModalCount > 0) {
-      openModalCount -= 1;
-      if (openModalCount === 0) {
-        document.body.style.overflow = '';
-      }
-    }
-
+    if (!isOpened) return;
+    document.body.style.overflow = 'hidden';
     return () => {
-      openModalCount -= 1;
-      if (openModalCount === 0) {
-        document.body.style.overflow = '';
-      }
+      document.body.style.overflow = '';
     };
   }, [isOpened]);
 }
