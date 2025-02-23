909cb590130868b8deeb37a8932c6c388e83d1a3
YSP97 <pus9717@gmail.com>
2025-02-23 12:27:57 +0900

    [Feat] favicon 설정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Health Care Application Fitculator" />
+        <meta name='robots' content="index"/>
+        <link rel="icon" type="image/png" href="/assets/icon.png" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
