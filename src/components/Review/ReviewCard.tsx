040dba2c560bb7184091332b93f4b09638306fde
채소 <chlthgus0901@naver.com>
2025-02-24 10:05:16 +0900

    [Modify] 리뷰 mockReviewList 수정

--- src/components/Review/ReviewCard.tsx
+++ src/components/Review/ReviewCard.tsx
@@ -10,13 +10,18 @@ type ReviewProps = {
 
 function ReviewCard({ id, img, rating, content }: ReviewProps) {
   return (
-    <article className="flex max-h-[9.5rem] flex-col gap-4 self-stretch rounded-xs bg-gray-50 p-4 md:max-w-[35.125rem]">
+    <article className="flex flex-col gap-4 self-stretch rounded-xs bg-gray-50 p-4 md:max-w-[35.125rem]">
       <div className="flex items-center justify-between gap-1 self-stretch overflow-hidden">
-        <div className="flex gap-2">
+        <div className="flex items-center gap-2">
           <div className="relative h-8 w-8 overflow-hidden rounded-full bg-gray-400">
-            <Image src={img} alt={img} fill className="absolute object-cover" />
+            <Image
+              src={img}
+              alt={`${id}의 프로필이미지`}
+              fill
+              className="absolute object-cover"
+            />
           </div>
-          <span className="overflow-hidden overflow-ellipsis text-base font-normal leading-[140%] text-white">
+          <span className="overflow-hidden overflow-ellipsis text-base text-white">
             {id}
           </span>
         </div>
@@ -35,7 +40,15 @@ function ReviewCard({ id, img, rating, content }: ReviewProps) {
         </div>
       </div>
 
-      <span className="overflow-hidden text-[1.125rem] font-normal leading-[140%] text-white">
+      <span
+        className="max-h-[4.5rem] overflow-hidden overflow-ellipsis text-[1.125rem] font-normal leading-[140%] text-white"
+        style={{
+          display: '-webkit-box',
+          WebkitBoxOrient: 'vertical',
+          WebkitLineClamp: 3,
+          overflow: 'hidden',
+        }}
+      >
         {content}
       </span>
     </article>
