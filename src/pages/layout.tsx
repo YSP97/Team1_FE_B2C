b759df55ac3d5cf846612e3912645c62da14c819
YSP97 <pus9717@gmail.com>
2025-02-24 11:09:04 +0900

    [Modify] Title 설정

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -2,6 +2,7 @@ import localFont from 'next/font/local';
 import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer/Footer';
 import React from 'react';
+import Head from 'next/head';
 
 const pretendard = localFont({
   src: '../fonts/PretendardVariable.woff2',
@@ -12,11 +13,17 @@ const pretendard = localFont({
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className={`${pretendard.variable} bg-bg-primary font-pretendard`}>
-      <Header />
-      <main className="mt-[3rem] md:mt-[3.75rem]">{children}</main>
-      <Footer />
-    </div>
+    <>
+      <Head>
+        <title>Fitculator</title>
+        <meta name="description" content="Health Care Application Fitculator" />
+      </Head>
+      <div className={`${pretendard.variable} bg-bg-primary font-pretendard`}>
+        <Header />
+        <main className="mt-[3rem] md:mt-[3.75rem]">{children}</main>
+        <Footer />
+      </div>
+    </>
   );
 };
 
