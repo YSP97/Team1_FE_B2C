b2d14f862665c39d7a371c517df18232f155125f
Gomyeunghan <rhaudgks04@gmail.com>
2025-02-25 22:21:23 +0900

    [test] lottie 오류수정하기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
 import AppBanner from '@/components/AppBanner';
 import Apply from '@/components/Apply';
 import Community from '@/components/Community';
-import Detailed from '@/components/Detailed';
 import Faq from '@/components/Faq/Faq';
 import Review from '@/components/Review/Review';
 import dynamic from 'next/dynamic';
 
 const AppIntroduce = dynamic(() => import('../components/AppIntroduce'), {
+  ssr: false,
+});
+const Detailed = dynamic(() => import('../components/Detailed'), {
   ssr: false,
 });
 
