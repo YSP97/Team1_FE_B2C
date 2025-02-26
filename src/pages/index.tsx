d0398f201134226af07ccad7675d862e60ca1f95
Gomyeunghan <rhaudgks04@gmail.com>
2025-02-26 20:11:13 +0900

    [Feature] 스크롤 트레킹 코드작성

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,8 @@ import Community from '@/components/Community';
 import Faq from '@/components/Faq/Faq';
 import Review from '@/components/Review/Review';
 import dynamic from 'next/dynamic';
+import useSectionTracking from '@/hooks/useSectionTracking';
+import useSectionTime from '@/hooks/useSectionTime';
 
 const AppIntroduce = dynamic(() => import('../components/AppIntroduce'), {
   ssr: false,
@@ -13,16 +15,41 @@ const Detailed = dynamic(() => import('../components/Detailed'), {
 });
 
 export default function Home() {
+  useSectionTracking('app-banner');
+  useSectionTracking('apply');
+  useSectionTracking('community');
+  useSectionTracking('faq');
+  useSectionTracking('review');
+
+  useSectionTime('app-banner');
+  useSectionTime('apply');
+  useSectionTime('community');
+  useSectionTime('faq');
+  useSectionTime('review');
   console.log('index');
   return (
     <>
-      <AppBanner />
-      <AppIntroduce />
-      <Detailed />
-      <Community />
-      <Review />
-      <Apply />
-      <Faq />
+      <section id="app-banner">
+        <AppBanner />
+      </section>
+      <section id="app-introduce">
+        <AppIntroduce />
+      </section>
+      <section id="detailed">
+        <Detailed />
+      </section>
+      <section id="community">
+        <Community />
+      </section>
+      <section id="review">
+        <Review />
+      </section>
+      <section id="apply">
+        <Apply />
+      </section>
+      <section id="faq">
+        <Faq />
+      </section>
     </>
   );
 }
