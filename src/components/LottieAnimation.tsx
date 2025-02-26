acb82ed0bc22d1288dec5a29edb51a8b00b2a550
Gomyeunghan <rhaudgks04@gmail.com>
2025-02-26 09:54:04 +0900

    [Modify] 빌드시 이미지 수정

---
+++ src/components/LottieAnimation.tsx
@@ -0,0 +1,8 @@
+import Lottie from 'lottie-react';
+import LottieData from '@/mockData/LottieData.json';
+
+function LottieAnimation() {
+  return <Lottie animationData={LottieData}></Lottie>;
+}
+
+export default LottieAnimation;
